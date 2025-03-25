aa8f0291fe32a939cd0a28f420ebbecf17157e0b
KimberlyA <kimberlyarango474@gmail.com>
2025-03-25 16:08:08 -0500

    septimo cargo

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function Loguear() {
     let username = document.getElementById('username').value
     let password = document.getElementById('clave').value
 
-    if(username == 'Rodolfo' && password == '123456'){
+    if(username == 'Humberto' && password == '142536'){
         alert('Usuario valido')
     }else{
         alert('usuario incorrecto')
